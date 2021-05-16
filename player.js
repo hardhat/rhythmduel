deafb090ea1074a59dac8746650ab7f05db0975e
DeathLemons <64559878+DeathLemons@users.noreply.github.com>
2021-05-16 01:10:48 -0400

    patterns
    pattern check function made still needs more work

--- player.js
+++ player.js
@@ -2,20 +2,11 @@
 import Actor from './actor.js'
 // Listens for rhythm based commands and updates Actor class
 
-var scene;
-//var comboCount = 0;
-var key1 = false;
-var key2 = false;
-var key3 = false;
-var key4 = false;
-//var comboString = "";
-
 export default class Player extends Actor {
     constructor ({ scene, sprite, x, y, health }) {
         super({ scene, sprite, x, y, health });
 
         this.scene = scene;
-        //this.comboCount = 0;
     }
 
     create()
@@ -26,25 +17,21 @@ export default class Player extends Actor {
         console.log('key1');
         this.comboCount += 1;
         this.comboString += "1";
-        key1 = true;
       }, this);
       this.scene.input.keyboard.on('keydown-DOWN', function(event){
         console.log('key2');
         this.comboCount += 1;
         this.comboString += "3";
-        key2 = true;
       }, this);
       this.scene.input.keyboard.on('keydown-LEFT', function(event){
         console.log('key3');
         this.comboCount += 1;
         this.comboString += "4";
-        key3 = true;
       }, this);
       this.scene.input.keyboard.on('keydown-RIGHT', function(event){
         console.log('key4');
         this.comboCount += 1;
         this.comboString += "2";
-        key4 = true;
       }, this);
       this.scene.input.keyboard.on('keydown-C', function(event){
         console.log(this.comboCount);
@@ -54,11 +41,24 @@ export default class Player extends Actor {
       }, this);
     }
 
+    patternCheck()
+    {
+        this.patterns = ["1112","1212","2121","2221","3334","2424","4434"]
+        for(var i = 0; i < this.patterns.length; i++){
+        }
+        if(this.comboString == this.patterns[0]){
+          console.log('advance');
+          this.comboString = "";
+        }
+    }
+
     update ()
     {
+      this.patternCheck();
       if(this.comboCount == 4){
         console.log(this.comboString);
         this.comboCount = 0;
+        this.comboString = "";
       }
     }
 }
