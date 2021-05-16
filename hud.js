b2a7310476cb109272baf5841ee044b6ea91b3be
JetShark <jetshark02@gmail.com>
2021-05-16 16:15:05 -0400

    Fixed hud.js
    Hud.JS no longer throws any type based errors and successfully prints to show being updated.

--- hud.js
+++ hud.js
@@ -15,42 +15,58 @@ export default class Hud extends Phaser.GameObjects.Group {
         this.bg = this.scene.add.image(0,0,'hudBg').setOrigin(0, 0);
         this.width = 800;
 
-        //this.healthbar = new Phaser.Sprite(this.scene, 2, 2, 'healthbar');
-        this.healthbar = this.add.sprite(2,2,'healthbar').setOrigin(0, 0);
-        this.healthbar.scale.setTo(0.995, 11);
+        //this.healthbar = new Phaser.Sprite(this.scene, 2, 2, 'healthbar').setOrigin(0, 0);
+        this.healthbar = scene.add.sprite(2,2,'healthbar').setOrigin(0, 0);
+        //this.healthbar.scale.setTo(0.995, 11);
 
          this.score = 0;
+         //this.score.pts = int;
+         //this.score.pts = 0;
          this.scoreLabel = 'Score: ';
-         this.scoreText = new Phaser.Text(this.scene, 20, 14, this.scoreLabel + this.score, {
+
+        this.scoreText = '';
+        //this.scoreText.text = "";
+         /*this.scoreText = new Phaser.Text(this.scene, 20, 14, this.scoreLabel + this.score, {
              font: '13px Verdana',
              fill: 'white',
              align: 'center'
 
         });
+        */
 
-        this.scene.add(this.bg);
+        /*this.scene.add(this.bg);
         this.scene.add(this.healthbar);
         this.scene.add(this.scoreText);
+        */
 
         console.log('I Am the Very Model of a Modern Major General;');
     }
 
+    update ()
+    {
+        this.updateHealth();
+        this.updateScore();
+
+    }
+
     updateHealth ()
     {
-        this.healthbar.crop(new Phaser.Rectangle(0, 0, (this.player.health / this.player.maxHealth) * this.width, 10));
-        this.healthbar.updateCrop();
+        /*this.healthbar.crop(new Phaser.Rectangle(0, 0, (this.player.health / this.player.maxHealth) * this.width, 10));
+        this.healthbar.updateCrop();*/
     }
 
     updateScore(amount)
     {
         this.score += amount;
-        this.scoreText.text = this.scoreLabel + (this.score * 10);
+        //this.scoreText.text = this.scoreLabel + (this.score * 10);
+        this.scoreText = this.scoreLabel + (this.score * 10);
     }
-
+    /*
     updateScore(score) {
         this.score.pts += score;
         this.score.textObject.setText(('' + this.score.pts).padStart(6, '0'));
     }
+    */
     /*
     getPlayerHealth(player)
     {
