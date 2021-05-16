4ea8f843a6795ea9ba5d30618e5d5e50d09c82a4
DeathLemons <64559878+DeathLemons@users.noreply.github.com>
2021-05-15 21:14:48 -0400

    keypresses
    more keypress stuff still under work

--- player.js
+++ player.js
@@ -8,43 +8,36 @@ var leftKey;
 var rightKey;
 var scene;
 var comboCount = 0;
-
-
+var up = false;
+var down = false;
+var right = false;
+var left = false;
 
 export default class Player extends Actor {
     constructor ({ scene, x, y, texture, frame, health }) {
         super({ scene, x, y, texture, frame, health });
         this.scene = scene;
+
     }
-
-
 
     create()
     {
-      upKey = this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
-      downKey = this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
-      leftKey = this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
-      rightKey = this.scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+      this.scene.input.keyboard.on('keydown-UP', function(event){
+        console.log('yoy');
+      });
+      this.scene.input.keyboard.on('keydown-DOWN', function(event){
+        console.log('yey');
+      });
+      this.scene.input.keyboard.on('keydown-LEFT', function(event){
+        console.log('yry');
+      });
+      this.scene.input.keyboard.on('keydown-RIGHT', function(event){
+        console.log('yay');
+      });
     }
 
     update ()
     {
-      if(upKey.isDown){
-        //console.log('yoy');
-        comboCount = comboCount + 1;
-      }
-      if(downKey.isDown){
-        //console.log('yay');
-        comboCount = comboCount + 4;
-      }
-      if(leftKey.isDown){
-        //console.log('yey');
-        comboCount = comboCount + 3;
-      }
-      if(rightKey.isDown){
-        //console.log('ysy');
-        comboCount = comboCount + 2;
-      }
       if( comboCount == 5){
         console.log('advance');
         comboCount = 0;
