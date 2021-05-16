3e1d20f555eb71639775b9511ee294f95544e7a5
Dale Wick <dmwick@yahoo.com>
2021-05-15 23:34:38 -0400

    Worked on buttons and sounds

--- mainmenu.js
+++ mainmenu.js
@@ -23,21 +23,97 @@ export default class MainMenu extends Phaser.Scene {
         this.load.image('tojam', 'assets/menu/Button_ToJam.png');
         this.load.image('credits', 'assets/menu/credits.png');
 
+        this.load.audio('1', [ 'assets/syllables/DO_woman.wav', 'assets/syllables/DO_woman.mp3', 'assets/syllables/DO_woman.ogg' ]);
+        this.load.audio('2', [ 'assets/syllables/WAH_woman.wav', 'assets/syllables/WAH_woman.mp3', 'assets/syllables/WAH_woman.ogg' ]);
+        this.load.audio('3', [ 'assets/syllables/UHUH_woman.wav', 'assets/syllables/WAH_woman.mp3', 'assets/syllables/WAH_woman.ogg' ]);
+        this.load.audio('4', [ 'assets/syllables/KATTA_woman.wav', 'assets/syllables/WAH_woman.mp3', 'assets/syllables/WAH_woman.ogg' ]);
     }
 
     create ()  {
+        this.normalButtonList = [];
 
         this.add.image(0, 0, 'menu').setOrigin(0, 0);
-
-        this.add.image(400,300,'new');
-
-        // Should be replaced with gameObject.setInteractive(shape,callback) for buttons.
-        this.input.on('pointerup', function(pointer) {
-            var touchX = pointer.x;
-            var touchY = pointer.y;
-
+        var newGameButton = this.add.image(400,300,'new');
+        newGameButton.setInteractive();
+        newGameButton.on('clicked', function(item) {
             this.scene.start('Bout');
+        },this);
+        this.normalButtonList.push(newGameButton);
+        
+        var aboutButton = this.add.image(400,475,'about');
+        aboutButton.setInteractive();
+        aboutButton.on('clicked', function(item) {
+            this.hideNormalButtons();
+
+            var aboutWindow = this.add.image(400,300,'team');
+            aboutWindow.setInteractive();
+            aboutWindow.on('clicked',this.deleteItem,this);
+        },this);
+        this.normalButtonList.push(aboutButton);
+
+        //  If a Game Object is clicked on, this event is fired.
+        //  We can use it to emit the 'clicked' event on the game object itself.
+        this.input.on('gameobjectup', function (pointer, gameObject)
+        {
+            gameObject.emit('clicked', gameObject);
         }, this);
+
+
+        this.createSounds();
+    }
+
+    hideNormalButtons() {
+        this.normalButtonList.forEach(button => {
+            button.setVisible(false);
+            button.input.enabled = false;
+        });
+    }
+
+    showNormalButtons() {
+        this.normalButtonList.forEach(button => {
+            button.setVisible(true);
+            button.input.enabled = true;
+        });
+    }
+
+    deleteItem(item) {
+        item.off('clicked',this.deleteItem);
+        item.input.enabled = false;
+        item.setVisible(false);
+        this.showNormalButtons();
+    }
+
+    createSounds() {
+        var syllable1 = this.sound.add('1');
+        var syllable2 = this.sound.add('2');
+        var syllable3 = this.sound.add('3');
+        var syllable4 = this.sound.add('4');
+        console.log('Do Wa Uhuh Katta');
+    
+        this.input.keyboard.on('keydown-SPACE', function () {
+            console.log("Quiet.");
+            this.sound.stopAll();
+        }, this);
+    
+        this.input.keyboard.on('keydown-A', function () {
+            console.log("Do");
+            syllable1.play();
+        });
+    
+        this.input.keyboard.on('keydown-W', function () {
+            console.log("Wa");
+            syllable2.play();
+        });
+    
+        this.input.keyboard.on('keydown-S', function () {
+            console.log("Uhuh");
+            syllable3.play();
+        });
+    
+        this.input.keyboard.on('keydown-D', function () {
+            console.log("Katta");
+            syllable4.play();
+        });
     }
 
     update () {
